a7b9c3c73673240ef4e031c1becabc5953b47548
shizonghai <1966322701@qq.com>
2023-01-06 17:31:17 +0800

    新增了index.js和index.css 修改了index.html

---
+++ index.js
@@ -0,0 +1,11 @@
+var scenic = document.querySelectorAll(".scenic li");
+var picture = document.querySelectorAll(".picture li");
+for (var i = 0; i < scenic.length; i++) {
+    scenic[i].setAttribute("index", i);
+    scenic[i].addEventListener("click", function () {
+        for (var j = 0; j < picture.length; j++) {
+            picture[j].style.display = "none";
+        }
+        picture[this.getAttribute('index')].style = "block";
+    })
+}
